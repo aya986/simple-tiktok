468c773c13370dffcfcf91b5d7605d3f1ff6b70c
aya <ayoob.khodadadi@gmail.com>
2022-08-16 11:28:32 +0430

    implement tiktok-app-api

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+const tiktok = require('tiktok-app-api');
+
+let tiktokApp;
+
+(async () => {
+  tiktokApp = await tiktok();
+
+  const iterator = tiktokApp.getTrendingVideos();
+  const videosResult = await iterator.next();
+  const trendingVideos = videosResult.value;
+
+  console.log('====================================');
+  console.log(trendingVideos);
+  console.log('====================================');
+//   const tags = trendingVideos.map(v => v.tags);
+
+//   console.log(tags);
+})();
