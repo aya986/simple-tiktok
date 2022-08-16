6bbd526537906266c0f0f10bbed38c187613e004
aya <ayoob.khodadadi@gmail.com>
2022-08-16 09:32:19 +0430

    implement tiktok-scraper

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
 // eslint-disable-next-line no-console
+import TikTokScraper from "tiktok-scraper";
 console.log('Hello world!');
+
+(async () => {
+    try {
+        const posts = await TikTokScraper.user('USERNAME', {
+            number: 100,
+            sessionList: ['_ttp=61cdd9a7e233a8ec431f95f46f17754e;']
+        });
+        console.log(posts);
+    } catch (error) {
+        console.log(error);
+    }
+})();
